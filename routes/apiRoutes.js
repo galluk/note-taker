3e107fb9387bd7d47a7c2367278d485a5932d65b
HomeAlone <luke.gallagher.cst@gmail.com>
2020-11-01 20:23:54 +1100

    added note update functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+const fs = require('fs');
 const crypto = require('crypto');
 const { log } = require('console');
 
@@ -6,15 +6,15 @@ const { log } = require('console');
 // ROUTING
 // ===============================================================================
 
-let noteData = [];
+let noteItems = [];
 
 module.exports = function (app) {
     // API Requests
     // read the `db.json` file and return all saved notes as JSON
     app.get("/api/notes", function (req, res) {
         let dbContents = fs.readFileSync(__dirname + "/../db/db.json");
-        noteData = JSON.parse(dbContents);
-        res.send(noteData);
+        noteItems = JSON.parse(dbContents);
+        res.send(noteItems);
     });
 
     // Receive a new note to save on the request body
@@ -24,22 +24,49 @@ module.exports = function (app) {
         newNote.id = crypto.randomBytes(8).toString("hex");
 
         // add it to the `db.json` file
-        noteData.push(newNote);
-        fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteData), "UTF8");
+        noteItems.push(newNote);
+        fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
 
         // return the new note to the client.
         res.send(newNote);
     });
 
+    // Receive a note to update on the request body
+    app.put("/api/notes", function (req, res) {
+        let updateNote = req.body;
+
+        //only continue with update if the id is valid
+        if (updateNote.id) {
+            let savedNote = noteItems.find(({ id }) => { return id === updateNote.id })
+
+            if (savedNote) {
+
+                // add it to the `db.json` file
+                savedNote.title = updateNote.title;
+                savedNote.text = updateNote.text;
+
+                fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
+
+                // return the new note to the client.
+                res.send(updateNote);
+
+            } else {
+                res.json({ ok: false, result: 'no note with provided id found' });
+            }
+        } else {
+            res.json({ ok: false, result: 'id not sent' });
+        }
+    });
+
     // receive a query parameter containing the id of a note to delete. 
     app.delete("/api/notes/:id", function (req, res) {
         // remove the note with the given id 
-        noteData = noteData.filter((item) => {
+        noteItems = noteItems.filter((item) => {
             return item.id !== req.params.id;
         });
 
         // rewrite the notes to the `db.json` file.
-        fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteData), "UTF8");
+        fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
 
         res.json({ ok: true });
     });
