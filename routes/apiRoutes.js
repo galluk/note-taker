8598af972bc1ed98d1c13abba9fbf81bfba6738c
HomeAlone <luke.gallagher.cst@gmail.com>
2020-11-02 16:53:34 +1100

    updates for error response and code tidy up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
-const { log } = require('console');
+const e = require('express');
 
 // ===============================================================================
 // ROUTING
@@ -10,6 +10,7 @@ let noteItems = [];
 
 module.exports = function (app) {
     // API Requests
+
     // read the `db.json` file and return all saved notes as JSON
     app.get("/api/notes", function (req, res) {
         let dbContents = fs.readFileSync(__dirname + "/../db/db.json");
@@ -23,11 +24,16 @@ module.exports = function (app) {
         // assign the note a unique id
         newNote.id = crypto.randomBytes(8).toString("hex");
 
-        // add it to the `db.json` file
+        if (!newNote.title || !newNote.text) {
+            // incomplete input data so return error
+            return res.status(400).json({ msg: 'Please include a title and text for the note' });
+        }
+
+        // valid data so add it to the `db.json` file
         noteItems.push(newNote);
         fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
 
-        // return the new note to the client.
+        // return the new note 
         res.send(newNote);
     });
 
@@ -51,23 +57,35 @@ module.exports = function (app) {
                 res.send(updateNote);
 
             } else {
-                res.json({ ok: false, result: 'no note with provided id found' });
+                // send error code with message
+                return res.status(400).json({ msg: `no note with id (${updateNote.id}) found` });
             }
         } else {
-            res.json({ ok: false, result: 'id not sent' });
+            // send error code with message
+            return res.status(400).json({ msg: 'no id provided' });
         }
     });
 
     // receive a query parameter containing the id of a note to delete. 
     app.delete("/api/notes/:id", function (req, res) {
-        // remove the note with the given id 
-        noteItems = noteItems.filter((item) => {
-            return item.id !== req.params.id;
-        });
+        if (noteItems.some(item => item.id === req.params.id)) {
+            // remove the note with the given id from the array
+            noteItems = noteItems.filter((item) => {
+                return item.id !== req.params.id;
+            });
 
-        // rewrite the notes to the `db.json` file.
-        fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
+            // rewrite the notes to the `db.json` file.
+            fs.writeFileSync(__dirname + "/../db/db.json", JSON.stringify(noteItems), "UTF8");
 
-        res.json({ ok: true });
+            // set res and return the updated array
+            res.json({
+                msg: `note with id (${req.params.id}) successfully deleted`,
+                members: noteItems
+            });
+        }
+        else {
+            // send error code with message
+            res.status(400).json({ msg: `no note with id (${req.params.id}) found` });
+        }
     });
 };
